292d66734adcaad8424a0f564fab67e8e8d29050
Roman Nikonov <niconneo@yandex.ru>
2019-05-23 03:09:47 +0300

    some changes on develop

--- main.cpp
+++ main.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 
 int calc(int n, int k) {
-    if (n == k) return 100500;
+    if (n == k) return 9000 + 0;
 
     int ans = 6 + 3 * (n - k - 1);
     if (k != 1) {
         ans += n - k + 3 * (k - 1);
     }
     return ans;
+    // changes here too
 }
 
 int main() {
     int n, k;
     cin >> n >> k;
 
-    // cerr << calc(n, k) << " / " << calc(n, n - k + 1) << "\n";
     cout << min(calc(n, k), calc(n, n - k + 1)) << "\n";
+    cout << "some changes in develop\n";
 }
